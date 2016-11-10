ab4d63ba630c4b30b0c9e42f4c1d730eb5a0846b
Matteo Cerutti <matteo.cerutti@rkrepsilon.com>
2016-11-10 10:05:56 +0000

    fixed path to settings.py.

--- files/wsgi.py
+++ files/wsgi.py
@@ -2,5 +2,5 @@ from __future__ import absolute_import
 import os
 
 # Needed if a settings.py file exists
-os.environ['PUPPETBOARD_SETTINGS'] = '/app/settings.py'
+os.environ['PUPPETBOARD_SETTINGS'] = '/etc/puppetboard/settings.py'
 from puppetboard.app import app as application
