6c0983ab6dc29cefc742e796286165f3b6685235
Matteo Cerutti <matteo.cerutti@rkrepsilon.com>
2017-03-15 12:29:42 +0000

    moving puppetboard to uwsgi behind nginx and supervisord

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
-FROM alpine:3.4
+FROM centos:7
 MAINTAINER Matteo Cerutti <matteo.cerutti@hotmail.co.uk>
 
-ENV PUPPETBOARD_VERSION "0.2.1"
+ENV PUPPETBOARD_VERSION "0.2.0"
 
-RUN apk add --no-cache py-pip
-RUN pip install puppetboard==$PUPPETBOARD_VERSION gunicorn
+RUN yum clean all && yum install -y supervisor nginx uwsgi uwsgi-plugin-python && rm -rf /var/cache/yum/* && pip install puppetboard==$PUPPETBOARD_VERSION && rm -rf /root/.cache
 
-COPY files/run.sh /
-RUN mkdir -p /app /etc/puppetboard
+COPY files/supervisor.ini /etc/supervisord.d/puppetboard.ini
 COPY files/settings.py /etc/puppetboard/
 COPY files/wsgi.py /app/
+COPY files/wsgi.ini /app/
+COPY files/nginx.conf /etc/nginx/
 
 EXPOSE 8000
 
 WORKDIR /app
-CMD ["/run.sh"]
+CMD ["/usr/bin/supervisord", "-n", "-c", "/etc/supervisord.conf"]
--- files/wsgi.py
+++ files/wsgi.py
@@ -1,6 +1,22 @@
 from __future__ import absolute_import
 import os
 
+class PrefixMiddleware(object):
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+        print(environ['PATH_INFO'])
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["This url does not belong to the app.".encode()]
+
 # Needed if a settings.py file exists
 os.environ['PUPPETBOARD_SETTINGS'] = '/etc/puppetboard/settings.py'
-from puppetboard.app import app as application
+from puppetboard.app import app
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/puppetboard')
